13f165757d2733c65b05bf79dddfd8d681f9c5cf
Mike Lynch <mjlynch2@gmail.com>
2022-01-13 06:33:53 -0600

    configure app for deploy

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
-const express = require("express"),
-  bodyParser = require("body-parser"),
-  cors = require("cors"),
-  Board = require("./board.js");
+const express     = require("express"),
+      bodyParser  = require("body-parser"),
+      cors        = require("cors"),
+      path        = require("path"),
+      Board       = require("./board.js");
+
 const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(cors());
+app.use(express.static(path.resolve(__dirname, "../build")));
+
 
 let ca = new Board(25, 500);
 
